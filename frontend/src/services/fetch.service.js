b089bb57e2892dee41e8f4fa9a5b752bca3e0da2
Zen 0ne <gor.sharoyan95@gmail.com>
2025-12-21 22:54:02 +0400

    :construciton_worker:build: created fetch services in order to post and rescive data

---
+++ frontend/src/services/fetch.service.js
@@ -0,0 +1,33 @@
+// Main Fetch function
+
+async function fetchData(method, api, body = null) {
+  try {
+    const response = await fetch(api, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: body ? JSON.stringify(body) : null // only send body if provided
+    });
+
+    if (!response.ok) {
+      throw new Error("Problem with fetching data");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+// GET fetch function
+export function getData(api) {
+  return fetchData("GET", api);
+}
+
+// POST fetch function
+export function postData(api, body) {
+  return fetchData("POST", api, body);
+}
